ebfcb4144b52c3c5a194cc19f7e252cef463608e
Ngo Van Thien <thienbg93@gmail.com>
2015-06-27 00:07:06 +0700

    Finish log-in log-out

---
+++ db/seeds.rb
@@ -0,0 +1,20 @@
+User.create!(name: "ngothien",
+             email: "thien@gmail.com",
+             password: "abc123",
+             password_confirmation: "abc123",
+             activated: true,
+             activated_at: Time.zone.now)
+
+User.create!(name: "nguyenson",
+             email: "sonn@gmail.com",
+             password: "abc123",
+             password_confirmation: "abc123",
+             activated: true,
+             activated_at: Time.zone.now)
+
+User.create!(name: "nguyenhao",
+             email: "hao@gmail.com",
+             password: "abc123",
+             password_confirmation: "abc123",
+             activated: true,
+             activated_at: Time.zone.now)
