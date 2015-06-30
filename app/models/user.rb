db688eebf0466e08fe2358487d8516c8f7f3b179
nhs3108 <nhs3108@gmail.com>
2015-06-30 14:28:09 +0700

    Signup and update account

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token
-  before_save   :downcase_email
-  before_save {self.email = email.downcase}
-  validates :name,  presence: true, length: {maximum: Settings.maximum_name_length}
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: {maximum: Settings.maximum_email_length},
-                    format: {with: VALID_EMAIL_REGEX},
-                    uniqueness: {case_sensitive: false}
-  has_secure_password
-  validates :password, presence: true, length: {minimum: Settings.minimum_password_length}, allow_nil: true
+  attr_accessor :remember_token, :activation_token, :reset_token
   has_many :activities, dependent: :destroy
   has_many :lessons, dependent: :destroy
   has_many :active_relationships,  class_name:  "Relationship",
@@ -19,10 +10,28 @@ class User < ActiveRecord::Base
                                  dependent: :destroy
   has_many :following, through: :active_relationships,  source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
+  mount_uploader :avatar, AvatarUploader
+  has_secure_password
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, presence: Settings.user.name.is_presence,
+                  length: {minimum: Settings.user.name.min_size,
+                          maximum: Settings.user.name.max_size}
+  validates :email, presence: Settings.user.email.is_presence,
+                    uniqueness: Settings.user.email.is_unique,
+                    format: {with: VALID_EMAIL_REGEX},
+                    length: {maximum: Settings.user.email.max_size}
+  validates :password, presence: Settings.user.password.is_presence,
+                      allow_blank: Settings.user.password.is_allow_blank,
+                      length: {minimum: Settings.user.password.min_size,
+                              maximum: Settings.user.password.max_size}
+  validates :avatar, presence: Settings.user.avatar.is_presence,
+                    allow_blank: Settings.user.avatar.is_allow_blank
+  validate :avatar_size
+  before_save :downcase_email
+  before_create :create_activation_digest
 
   def User.digest string
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
@@ -30,22 +39,63 @@ class User < ActiveRecord::Base
     SecureRandom.urlsafe_base64
   end
 
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password? token
+  end
+
   def remember
     self.remember_token = User.new_token
-    update_attribute(:remember_digest, User.digest(remember_token))
+    update_attributes :remember_digest, User.digest(remember_token)
   end
 
   def forget
-    update_attribute(:remember_digest, nil)
+    update_attributes :remember_token, nil
   end
 
-  def authenticated? remember_token
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def follow other_user
+    active_relationships.create followed_id: other_user.id
   end
-  
+
+  def unfollow other_user
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following? other_user
+    following.include? other_user
+  end
+
+  def activate
+    update_attributes activated: true, activated_at: Time.zone.now
+  end
+
+  def password_reset_expired?
+    reset_at < Settings.reset_pass_expered_in_hour.hours.ago
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attributes reset_digest: User.digest(reset_token), reset_at: Time.zone.now
+  end
+
   private
   def downcase_email
     self.email = email.downcase
+  end
+
+  def avatar_size
+    if avatar.size > Settings.user.avatar.max_size_in_MB.megabytes
+      errors.add :avatar, I18n.t("should_be_less_than_x_unit", x: Settings.user.avatar.max_size_in_MB, unit: Settings.user.avatar.unit)
+    end
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest activation_token
   end
 end
