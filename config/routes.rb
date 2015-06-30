faf58cdfd38a9d248666029861d572bf040826e4
Ngo Van Thien <thienbg93@gmail.com>
2015-06-30 15:26:13 +0700

    Show all users and user profile

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,10 @@ Rails.application.routes.draw do
   delete "logout"  => "sessions#destroy"
 
   resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :relationships,       only: [:create, :destroy]
 end
