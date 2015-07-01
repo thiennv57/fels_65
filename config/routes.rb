8e2bb559be978b372c2c4f302909eefd2e0817a6
nhs3108 <nhs3108@gmail.com>
2015-07-01 09:23:19 +0700

    Add admin namespace, add dashboard controller and dashboard page which is admin root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    root "dashboard#index"
+  end
+
   root "static_pages#home"
-  get "about"   => "static_pages#about"
-  get "sessions/new"
-  get "users/new"
-  get "signup"  => "users#new"
-  get    "login"   => "sessions#new"
-  post   "login"   => "sessions#create"
-  delete "logout"  => "sessions#destroy"
+  get "about" => "static_pages#about"
+  get "signup" => "users#new"
+  get "login" => "sessions#new"
+  get "login" => "sessions#new"
+  post "login" => "sessions#create"
+  delete "logout" => "sessions#destroy"
 
   resources :users
   resources :account_activations, only: [:edit]
