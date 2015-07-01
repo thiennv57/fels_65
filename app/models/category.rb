08512c165c3d61f828830143d4b87ea66e2117d7
nhs3108 <nhs3108@gmail.com>
2015-07-01 11:21:43 +0700

    Create and delete category using ajax

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ActiveRecord::Base
   has_many :words, dependent: :destroy
   has_many :lessons, dependent: :destroy
+  validates :name, length: {maximum: Settings.category.name.max_size}
 end
