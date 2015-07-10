7a9cc32604d3408d5c6e71432bce89a63831b651
Ngo Van Thien <thienbg93@gmail.com>
2015-07-10 10:25:05 +0700

    Edit Category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,4 +4,10 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.paginate(page: params[:page], per_page: Settings.paginate_per_page).order :name
   end
+  
+  def show
+    @category = Category.find params[:id]
+    @words = Word.in_category(params[:category_id]).not_learned(current_user)
+      .paginate page: params[:page], per_page: Settings.paginate_per_page
+  end
 end
